f31578203989cbe4c574f23170aea2e01bb8c0b9
Jonah Ullman <jonahmullman@gmail.com>
2020-11-30 15:59:48 -0600

    Defines 2x2 grid for tablets and smaller

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -3,12 +3,16 @@ import React from 'react';
 import './Styles.scss';
 
 const Button = (props) => {
-
   return (
-    <div className={"submitButton " + (props.inactive ? "submitButton--inactive" : "")} onClick={props.handleSubmit} >
+    <div
+      className={
+        'submitButton ' + (props.inactive ? 'submitButton--inactive' : '')
+      }
+      onClick={props.handleSubmit}
+    >
       {props.label}
     </div>
-  )
-}
+  );
+};
 
 export default Button;
